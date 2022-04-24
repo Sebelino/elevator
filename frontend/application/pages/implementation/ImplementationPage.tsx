c8d7645fc1c4add23f62ee33e930ebc525d9c1c8
Sebastian Olsson <sebelino7@gmail.com>
2022-04-24 17:07:14 +0200

    feat: Add some frontend elevator state stub data

--- frontend/application/pages/implementation/ImplementationPage.tsx
+++ frontend/application/pages/implementation/ImplementationPage.tsx
@@ -9,6 +9,8 @@ class ImplementationPage extends React.Component {
             <>
                 <h2 className={css.title}>Elevator service</h2>
                 <h3 className={css.title}>Current state of elevators</h3>
+                <li>Elevator 1: 2</li>
+                <li>Elevator 2: 1</li>
                 <h3 className={css.title}>Which floor would you like to call an elevator to?</h3>
                 <ElevatorButton symbol={"1"}/>
                 <ElevatorButton symbol={"2"}/>
