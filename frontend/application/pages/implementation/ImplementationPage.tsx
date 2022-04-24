2caf4a74b41d28b23d4d356a355b5644ecca02b1
Sebastian Olsson <sebelino7@gmail.com>
2022-04-24 17:05:54 +0200

    feat: Add some more headers

--- frontend/application/pages/implementation/ImplementationPage.tsx
+++ frontend/application/pages/implementation/ImplementationPage.tsx
@@ -7,7 +7,9 @@ class ImplementationPage extends React.Component {
     public render() {
         return (
             <>
-                <h2 className={css.title}>Call an elevator to which floor?</h2>
+                <h2 className={css.title}>Elevator service</h2>
+                <h3 className={css.title}>Current state of elevators</h3>
+                <h3 className={css.title}>Which floor would you like to call an elevator to?</h3>
                 <ElevatorButton symbol={"1"}/>
                 <ElevatorButton symbol={"2"}/>
                 <ElevatorButton symbol={"3"}/>
