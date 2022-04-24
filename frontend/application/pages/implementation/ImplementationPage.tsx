ab3b5396b071cc0f158d21304eaee44f3f951b1e
Sebastian Olsson <sebelino7@gmail.com>
2022-04-24 17:04:05 +0200

    feat: Change header

--- frontend/application/pages/implementation/ImplementationPage.tsx
+++ frontend/application/pages/implementation/ImplementationPage.tsx
@@ -7,7 +7,7 @@ class ImplementationPage extends React.Component {
     public render() {
         return (
             <>
-                <h2 className={css.title}>Lägg implementationen här</h2>
+                <h2 className={css.title}>Call an elevator to which floor?</h2>
                 <ElevatorButton symbol={"1"}/>
                 <ElevatorButton symbol={"2"}/>
                 <ElevatorButton symbol={"3"}/>
